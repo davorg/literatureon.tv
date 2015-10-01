54c211b6659c34d5ee65a2ee9adfe171a7c9ab6e
Dave Cross <dave@dave.org.uk>
2015-10-01 20:30:37 +0100

    Added all the default views

--- Literature/lib/Literature.pm
+++ Literature/lib/Literature.pm
@@ -8,16 +8,31 @@ my $cfg = dancer_app->config;
 $cfg->{plugins}{DBIC}{default}{user} = $ENV{LIT_USER};
 $cfg->{plugins}{DBIC}{default}{pass} = $ENV{LIT_PASS};
 
+my %resources = qw[
+  actors Actor
+  authors Author
+  productions Production
+  works Work
+  fictional_characters FictionalCharacter
+];
+
+my $route_re = '/(' . join('|', keys %resources) . ')/';
+my $id_re    = $route_re . '(\d+)';
+
 get '/' => sub {
     template 'index';
 };
 
-get '/authors/' => sub {
-    template 'authors', { authors => resultset('Author') };
+get qr{^$route_re$} => sub {
+  my ($resource) = splat;
+  template $resource,
+    { $resource => resultset($resources{$resource}) };
 };
 
-get '/authors/:id' => sub {
-    template 'author', { author => resultset('Author')->find(param('id')) };
+get qr{$id_re} => sub {
+  my ($resource, $id) = splat;
+  template substr($resource, 0, -1),
+    { lc $resources{$resource} => resultset($resources{$resource})->find($id) };
 };
 
 true;
